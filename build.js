2aa91c88f43dac571fd49daff0a7003ee97e099b
Pickcle <348707253@qq.com>
2017-07-20 10:59:03 +0800

    convert md to html

--- build.js
+++ build.js
@@ -1,27 +1,33 @@
 const path = require('path')
 const fs = require('fs')
+const execSync = require('child_process').execSync
 
 const showdown = require('showdown')
 
 showdown.setFlavor('github')
 const converter = new showdown.Converter()
 
-const sourcePath = 'blogs'
+const sourcePath = '../pickcle.github.io/_posts'
 
 const publishPath = 'dist'
 
+execSync(`cp index.css ${publishPath}`)
+
 fs.readdir(sourcePath, function (err, files) {
   files.forEach((file, index) => {
+    const date = file.match(/\d+-\d+-\d+/)[0]
+    const fileName = file.match(/\d+-\d+-\d+-(.*)/)[1]
+    const title = file.match(/\d+-\d+-\d+-(.*)\.markdown/)[1]
+
     fs.readFile(`${sourcePath}/${file}`, (err, data) => {
-      const text = data.toString()
-      // const title = text.match(/title:\s{2}"(\w+)"/)
+      let text = data.toString()
+      text = text.replace(/---(\n.*){4}\n---/, `### ${date}`)
       const html = converter.makeHtml(text)
 
-      // console.log(html)
-
       fs.readFile('index.html', (err, indexData) => {
-        const indexText = indexData.toString()
-        const outputText = indexText.replace('{{ content }}', html)
+        let indexText = indexData.toString()
+        indexText = indexText.replace(/{{ title }}/g, title)
+        indexText = indexText.replace('{{ content }}', html)
 
         if (!fs.existsSync(publishPath)) {
           fs.mkdir(publishPath, (err) => {
@@ -29,13 +35,16 @@ fs.readdir(sourcePath, function (err, files) {
           })
         }
 
-        fs.writeFile(`${publishPath}/${file}.html`, outputText, (err) => {
-          // console.log(outputText)
+        fs.writeFile(`${publishPath}/${title}.html`, indexText, (err) => {
+          if (err) {
+            console.log(`build failed: ${title}.html, ${err}`)
+          } else {
+            console.log(`build succeeded: ${title}.html`)
+          }
         })
 
       })
     })
   })
 
-  console.log(files)
 })
